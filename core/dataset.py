cd0f17c23f478678392be450687a9927985eb4c0
MeriDK <ma.rizhko@gmail.com>
2023-11-16 21:13:39 -0500

    Added errors to the dataset

--- core/dataset.py
+++ core/dataset.py
@@ -4,12 +4,23 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class MachoDataset(Dataset):
-    def __init__(self, data_root, prediction_length, mode='train'):
+    def __init__(self, data_root, prediction_length, mode='train', use_errors=False):
         data = joblib.load(data_root + f'{mode}.pkl')
+
         self.prediction_length = prediction_length
+        self.use_errors = use_errors
+
+        if use_errors and data[0].shape[1] != 3:
+            raise Exception("use_errors was True but dataset does not contain errors. "
+                            "Try running preprocess_data.py with the flag --use-error")
 
         self.times = data[0][:, 0, :]
-        self.values = data[0][:, 1, :]
+
+        if use_errors:
+            self.values = data[0][:, 1:, :]
+        else:
+            self.values = data[0][:, 1, :]
+
         self.aux = data[1]
         self.labels = data[2]
 
@@ -19,8 +30,14 @@ class MachoDataset(Dataset):
     def __getitem__(self, idx):
         past_times = torch.tensor(self.times[idx, :-self.prediction_length], dtype=torch.float)
         future_times = torch.tensor(self.times[idx, -self.prediction_length:], dtype=torch.float)
-        past_values = torch.tensor(self.values[idx, :-self.prediction_length], dtype=torch.float)
-        future_values = torch.tensor(self.values[idx, -self.prediction_length:], dtype=torch.float)
+
+        if self.use_errors:
+            past_values = torch.tensor(self.values[idx, :, :-self.prediction_length], dtype=torch.float).T
+            future_values = torch.tensor(self.values[idx, :, -self.prediction_length:], dtype=torch.float).T
+        else:
+            past_values = torch.tensor(self.values[idx, :-self.prediction_length], dtype=torch.float)
+            future_values = torch.tensor(self.values[idx, -self.prediction_length:], dtype=torch.float)
+
         past_mask = torch.ones(past_times.shape, dtype=torch.float)
         future_mask = torch.ones(future_times.shape, dtype=torch.float)
         aux = torch.tensor(self.aux[idx], dtype=torch.long)
