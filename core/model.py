50b9758a34b292541869cbf834075730872f77d8
MeriDK <ma.rizhko@gmail.com>
2024-01-18 14:47:04 -0500

    Added model or path to weight option

--- core/model.py
+++ core/model.py
@@ -3,11 +3,18 @@ from transformers import TimeSeriesTransformerForPrediction
 
 
 class ClassificationModel(nn.Module):
-    def __init__(self, pretrained_model_path, device, num_labels=8):
+    def __init__(self, device, pretrained_model=None, pretrained_model_path=None, num_labels=8):
         super(ClassificationModel, self).__init__()
 
-        self.pretrained_model = TimeSeriesTransformerForPrediction.from_pretrained(pretrained_model_path)
-        print(f'Loaded TimeSeriesTransformer from {pretrained_model_path}')
+        assert (pretrained_model is None) != (pretrained_model_path is None), \
+            'You must provide either pretrained_model or pretrained_model_path, but not both'
+
+        if pretrained_model:
+            self.pretrained_model = pretrained_model
+        else:
+            self.pretrained_model = TimeSeriesTransformerForPrediction.from_pretrained(pretrained_model_path)
+            print(f'Loaded TimeSeriesTransformer from {pretrained_model_path}')
+
         self.pretrained_model.to(device)
         self.device = device
 
@@ -15,12 +22,14 @@ class ClassificationModel(nn.Module):
         self.classifier.to(self.device)
 
     def forward(self, past_times, past_values, future_times, past_mask, aux):
+        static_real_features = aux.to(self.device) if self.pretrained_model.config.num_static_real_features else None
+
         outputs = self.pretrained_model(
             past_time_features=past_times.to(self.device),
             past_values=past_values.to(self.device),
             future_time_features=future_times.to(self.device),
             past_observed_mask=past_mask.to(self.device),
-            static_real_features=aux.to(self.device)
+            static_real_features=static_real_features
         )
 
         # embedding = torch.mean(outputs.encoder_last_hidden_state, dim=1)
