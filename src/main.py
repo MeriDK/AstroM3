c016220c5d26951d72c7601eb5a98e8152630b82
MeriDK <ma.rizhko@gmail.com>
2025-03-03 16:49:11 -0800

    Optimized dataset loading, updated CLIP weights loading

--- src/main.py
+++ src/main.py
@@ -4,29 +4,22 @@ import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from datetime import datetime
-from datasets import load_dataset
-import yaml
 import argparse
 
-from dataset import HFPSMDataset
+from data import get_datasets
 from loss import CLIPLoss
 from trainer import Trainer
-from utils import get_model, get_schedulers, set_random_seeds
+from utils import get_schedulers, set_random_seeds, load_config
+from model import get_model, load_clip_weights
 
-
-def load_config(config_path):
-    """
-    Load model and training configurations from a YAML file.
-
-    Args:
-        config_path (str): Path to the YAML configuration file.
-
-    Returns:
-        dict: Parsed configuration dictionary.
-    """
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-    return config
+# Dictionary for pre-trained CLIP weights paths
+CLIP_WEIGHTS = {
+    42: './weights/2025-02-27-19-55-6jb7me31/weights-best.pt',
+    66: './weights/2025-02-27-20-52-6vu1qioj/weights-best.pt',
+    0: './weights/2025-02-27-20-52-5ee7tjcc/weights-best.pt',
+    12: './weights/2025-02-27-19-55-et132353/weights-best.pt',
+    123: './weights/2025-02-27-19-55-ylaxu03p/weights-best.pt'
+}
 
 
 def run(config):
@@ -38,24 +31,26 @@ def run(config):
         config (dict): Dictionary containing model, dataset, and training configurations.
     """
     # Load the processed dataset from Hugging Face Hub
-    ds = load_dataset('MeriDK/AstroM3Processed', name=f'{config["data_sub"]}_{config["random_seed"]}_norm')
-
-    # Initialize train and validation datasets
-    train_dataset = HFPSMDataset(ds, classes=config['classes'], seq_len=config['seq_len'], split='train')
-    val_dataset = HFPSMDataset(ds, classes=config['classes'], seq_len=config['seq_len'], split='validation')
+    train, val, _ = get_datasets(config['dataset'],
+                                 name=f'{config["data_sub"]}_{config["random_seed"]}',
+                                 seq_len=config['seq_len'])
 
     # DataLoader for batch processing
-    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True,
+    train_dataloader = DataLoader(train, batch_size=config['batch_size'], shuffle=True,
                                   drop_last=True, num_workers=4)
-    val_dataloader = DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=False)
-
-    # Set device (GPU if available, otherwise CPU)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using', device)
+    val_dataloader = DataLoader(val, batch_size=config['batch_size'], shuffle=False)
 
     # Initialize the model based on the config
     model = get_model(config)
+
+    # Load CLIP pretrained weights if specified
+    if config['use_pretrain']:
+        model = load_clip_weights(model, config['pretrain_path'], config['mode'])
+
+    # Set device (GPU if available, otherwise CPU)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
+    print('Using', device)
 
     # Define optimizer (Adam with betas and weight decay)
     optimizer = Adam(model.parameters(), lr=config['lr'], betas=(config['beta1'], config['beta2']),
@@ -76,12 +71,12 @@ def run(config):
 
     # Evaluate the model on validation data if not in CLIP mode (for classification)
     if config['mode'] != 'clip':
-        trainer.evaluate(val_dataloader, id2target=train_dataset.id2label)
+        trainer.evaluate(val_dataloader, id2label=train.features['label'].names)
 
 
 def main():
     """
-    Parses command-line arguments, loads configurations, initializes training, and handles logging.
+    Parses command-line arguments, loads configurations, initializes training.
     """
     parser = argparse.ArgumentParser(description="AstroM3")
     parser.add_argument("--config", type=str, required=True, help="Path to the YAML config file")
@@ -91,12 +86,23 @@ def main():
     # Load YAML config file
     config = load_config(args.config)
 
-    # Append timestamp to the weights path to avoid overwriting previous runs
-    config['weights_path'] = os.path.join(config['weights_path'], datetime.now().strftime("%Y-%m-%d-%H-%M"))
-
     # Set random seed for reproducibility
     set_random_seeds(args.random_seed)
     config['random_seed'] = args.random_seed
+
+    # Selects the pre-trained CLIP weights based on the random seed.
+    if config['use_pretrain']:
+        # If weights exist locally for the given random seed, use them
+        if os.path.exists(CLIP_WEIGHTS[config['random_seed']]):
+            config['pretrain_path'] = CLIP_WEIGHTS[config['random_seed']]
+        else:
+            # Otherwise, use the Hugging Face Hub model with the corresponding seed
+            config['pretrain_path'] = f"MeriDK/AstroM3-CLIP-{config['random_seed']}"
+            print(f"CLIP weights not found at {CLIP_WEIGHTS[config['random_seed']]}.\n"
+                  f"Using Hugging Face pretrained weights from {config['pretrain_path']}")
+
+    # Append timestamp to the weights path to avoid overwriting previous runs
+    config['weights_path'] = os.path.join(config['weights_path'], datetime.now().strftime("%Y-%m-%d-%H-%M"))
 
     # Initialize Weights & Biases (wandb) for logging if enabled
     if config['use_wandb']:
